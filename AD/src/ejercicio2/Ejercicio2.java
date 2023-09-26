bd9168faae5e956d7ba029a7226c01071d70858f
alu <alu@PC75>
2023-09-26 12:31:28 +0200

    Ejercicios de clase

---
+++ AD/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,19 @@
+package ejercicio2;
+
+/**
+ * Realiza un programa Java que muestre los ficheros de un directorio. El nombre
+ * del directorio se pasará al programa desde la línea de comandos al
+ * ejecutarlo. Modifica el programa anterior para que muestre los nombres de los
+ * ficheros de un directorio, cuya extensión coincida con la que se pase como
+ * segundo argumento.
+ * 
+ *
+ */
+
+public class Ejercicio2 {
+	
+	public static void main(String[]args) {
+		
+	}
+
+}
