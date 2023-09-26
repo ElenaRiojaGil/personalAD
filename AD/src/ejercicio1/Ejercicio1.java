6c6ddc48e570becc8389aa6c6370809b7167e5a1
alu <alu@PC75>
2023-09-26 11:48:13 +0200

    ejercicios de clase

---
+++ AD/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,16 @@
+package ejercicio1;
+
+import java.io.File;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File f1 = new File("D:\\Documentos\\ElenaRioja\\AD\\WORKSPACE\\ejemplosAD\\Ficheros\\escribe.txt");
+		File f2 = new File("D:\\Documentos\\ElenaRioja\\AD\\WORKSPACE\\ejemplosAD\\Ficheros\\patata.txt");
+		File d1 = new File("D:\\Documentos\\ElenaRioja\\AD\\WORKSPACE\\ejemplosAD\\Ficheros");
+		File d2 = new File("D:\\Documentos\\ElenaRioja\\AD\\WORKSPACE\\ejemplosAD\\Ficherospatata");
+
+	}
+
+}
