8cbfe9a3bba20573d85843c4748084d85ec7f042
alu <alu@PC75>
2023-09-28 13:21:13 +0200

    Ejercicios clase

--- AD/src/ejercicio2/Ejercicio2.java
+++ AD/src/ejercicio2/Ejercicio2.java
@@ -1,5 +1,7 @@
 package ejercicio2;
 
+import java.io.File;
+import java.util.Scanner;
 /**
  * Realiza un programa Java que muestre los ficheros de un directorio. El nombre
  * del directorio se pasará al programa desde la línea de comandos al
@@ -13,7 +15,18 @@ package ejercicio2;
 public class Ejercicio2 {
 	
 	public static void main(String[]args) {
+		String dir;
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Nombre del directorio");
+		dir=sc.nextLine();
+		File dire=new File(dir);
+		String[] lista=dire.list();
+		for (int i = 0; i < lista.length; i++) {
+			System.out.println(lista[i]);
+		}
+		
+		sc.close();
 	}
 
 }
---
+++ AD/src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,98 @@
+package ejercicio4;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * Escribe un programa Java que pida una serie de frases por teclado hasta que
+ * se inserte como frase la palabra “fin”. Dichas frases deberán guardarse en un
+ * fichero de texto. A continuación, el programa visualizará el contenido del
+ * fichero, frase por frase. Cada vez que se ejecute el programa, se tienen que
+ * descartar las frases que ya estaban escritas en el fichero. Realiza este
+ * ejercicio sin usar la clase BufferedReader.
+ * 
+ * 
+ *
+ */
+public class Ejercicio4 {
+	public static void main(String[] args) {
+
+		Ejercicio4 ej4 = new Ejercicio4();
+		File mifichero = new File("ejercicio4.txt");
+		if(!mifichero.exists()) {
+			ej4.escribirfichero(mifichero, "");
+		}
+
+		Scanner sc = new Scanner(System.in);
+		String linea = "";
+		System.out.println("que quieres escribir?");
+		while (!linea.equals("fin")) {
+
+			linea = sc.nextLine();
+			if(!ej4.comparar(mifichero, linea)) {
+				ej4.escribirfichero(mifichero, linea);
+			}else {
+				System.out.println("La frase ya esta en el fichero");
+			}
+
+		}
+		ej4.leerfichero(mifichero);
+
+	}
+
+	public void leerfichero(File file) {
+		try {
+			FileReader lector = new FileReader(file);
+			int caracter = lector.read();// leo un byte y paso al siguiente
+			while (caracter > 0) {
+				System.out.print((char) caracter);
+				caracter = lector.read();
+			}
+			lector.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void escribirfichero(File file,String linea) {
+		try {
+			// mifichero.createNewFile();
+			FileWriter escritor = new FileWriter(file, false);
+
+			/*
+			 * Scanner sc = new Scanner(System.in); String linea= sc.nextLine();
+			 */
+			escritor.write(linea);
+			escritor.close();
+			// ¿cómo hago para que no se sobreescribra? CAMBIAR FALSE POR TRUE
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	public boolean comparar(File file, String frase) {
+		try {
+		FileReader lector = new FileReader(file);
+		boolean finalfichero=false;
+		while(!finalfichero) {
+			int byteEntrada=lector.read();               
+			if (byteEntrada==-1){
+				finalfichero = true;                                      
+			}
+			
+			
+		}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
