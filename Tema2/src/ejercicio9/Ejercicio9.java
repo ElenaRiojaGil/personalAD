b08e009ca22fae8dc593070b311d4c73f6fb3398
ElenaRiojaGil <eriojita@gmail.com>
2023-11-23 23:02:50 +0100

    Actualizacion

--- Tema2/src/ejercicio9/Ejercicio9.java
+++ Tema2/src/ejercicio9/Ejercicio9.java
@@ -2,21 +2,31 @@ package ejercicio9;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
+/**
+ * Realiza un programa Java que utilice el driver o conector JDBC con la base de
+ * datos biblioteca, creada en SQLite o MySQL, y que modifique un socio
+ * existente de la tabla SOCIO. Los datos del socio a modificar son el domicilio
+ * y el teléfono y se introducirán por teclado. En el caso de que el socio a
+ * modificar no exista en la tabla SOCIO, se deberá mostrar un mensaje por
+ * pantalla.
+ * 
+ * @author elena rioja
+ *
+ */
 public class Ejercicio9 {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		Connection conexion = null;
+		Gestion gestion = new Gestion();
+
 		try {
 			// Crear una instancia del JDBC driver
 			Class.forName("com.mysql.jdbc.Driver");
-
 			// Especificar la url de la base de datos.
 			// parametro 1 = Driver que utilizamos y ruta y nombre de la base de datos
 			// jdbc:sqlite:
@@ -25,48 +35,38 @@ public class Ejercicio9 {
 
 			// Establecer una conexión usando el driver que crea el objeto Connection.
 			conexion = DriverManager.getConnection("jdbc:mysql://192.168.56.102/biblioteca", "elena", "Pass!123456");
-			Statement sentencia = (Statement) conexion.createStatement();
-			ResultSet resultado = sentencia.executeQuery("select * from socio");
-				
-			 //Recibir los resultados en el objeto ResultSet.
-			 while(resultado.next()) {
-				
-				 System.out.println(resultado.getInt(1)+"-"+resultado.getString(2));
-			 }
-			// Crear un objeto Statement, usando Connection.
-			String socioModificar;
-			System.out.println("Escribe el nombre del socio a modificar:");
-			socioModificar = teclado.nextLine();
 
-			// Armar el postulado SQL y enviarlo a ejecución usando el Statement.
-			
-			ResultSet resultadoMod = sentencia.executeQuery("select * from socio WHERE `Nombre`=" + socioModificar);
 
-			// Recibir los resultados en el objeto ResultSet.
-			while (resultado.next()) {
-				int codigo = resultado.getInt("codigo");
-				System.out.println("Escribe el domicilio del socio a modificar:");
-				String domicilioModificar = teclado.nextLine();
-				System.out.println("Escribe el telefono del socio a modificar:");
-				int telefonoModificar = teclado.nextInt();
-				teclado.nextInt();
-				PreparedStatement pstmt = conexion.prepareStatement("UPDATE SOCIO SET Domicilio = ? WHERE Codigo = ?");
-				pstmt.setString(codigo, domicilioModificar);
-				pstmt = conexion.prepareStatement("UPDATE SOCIO SET Telefono = ? WHERE Codigo = ?");
-				pstmt.setInt(codigo, telefonoModificar);
-				int filas = pstmt.executeUpdate();
-				System.out.println("Correcto");
+			// Imprimimos primero el listado de los socios para que el usuario sepa cuales
+			// puede y no modificar.
 
+			System.out.println("Listado de todos los socios: ");
+			String consulta = "select * from socio";
+			gestion.lecturaDatos(conexion, consulta);
+
+			try {
+				// ya que puede haber varios socios con el mismo nombre
+				System.out.println("Introduzca el numero del socio que desea modificar: ");
+				int codigoSocio = teclado.nextInt();
+				teclado.nextLine();
+
+				if (!gestion.validarSocio(conexion, codigoSocio)) {
+					System.err.println("El codigo del socio " + codigoSocio + " no se ha encontrado.");
+				} else {
+					gestion.modificarDatos(conexion, codigoSocio);
+
+				}
+
+			} catch (InputMismatchException e) {
+				System.err.println("Error " + e.getMessage());
 			}
 
 			conexion.commit();
 			// cerrar los recursos
-			resultado.close();
-			sentencia.close();
 			conexion.close();
+			teclado.close();
 
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 			if (conexion != null) {
 				try {
@@ -77,7 +77,6 @@ public class Ejercicio9 {
 			}
 			System.err.println("Error de conexion");
 		}
-
 	}
 
 }
